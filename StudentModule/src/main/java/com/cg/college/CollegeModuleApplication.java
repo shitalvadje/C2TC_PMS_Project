bd99fb4eb43f2d01cfeb66ae9b4fc1d350d7fe9d
DELL <DELL@DESKTOP-QKAT4P8>
2022-11-07 12:45:34 +0530

    College Module

---
+++ StudentModule/src/main/java/com/cg/college/CollegeModuleApplication.java
@@ -0,0 +1,12 @@
+package com.cg.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeModuleApplication.class, args);
+	}
+}
---
+++ StudentModule/src/main/java/com/cg/college/CollegeRepository.java
@@ -0,0 +1,7 @@
+package com.cg.college;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CollegeRepository extends JpaRepository<College,Integer> {
+
+}
